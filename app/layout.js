0a3c0dbad7b47872364ae3f0e0bad712afbac8d9
codehass <hassan.elouardy06@gmail.com>
2025-02-23 17:33:10 +0000

    build home page

--- app/layout.js
+++ app/layout.js
@@ -18,14 +18,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return <html>
-    <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+    <body
+      className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
       <Header />
       <div className="flex-1 px-8 py-12">
         <main className="max-w-7xl mx-auto">
           {children}
         </main>
       </div>
-
     </body>
   </html>
 }
