e58a2d84437388f7cd1d0ad063fd4c281d67c613
codehass <hassan.elouardy06@gmail.com>
2025-03-04 17:18:17 +0000

    fetch number of cabins

--- app/about/page.js
+++ app/about/page.js
@@ -2,13 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 
 import about_1 from "@/public/about-1.jpg";
+import { getCabins } from "../_lib/data-service";
 // import about_2 from "@/public/about-2.jpg";
+
+export const revalidate = 86400;
 
 export const metadata = {
   title: "About"
 };
 
-export default function Page() {
+export default async function Page() {
+  const cabins = await getCabins();
+
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
@@ -17,7 +22,7 @@ export default function Page() {
         </h1>
 
         <div className="space-y-8">
-          {/* <p>
+          <p>
             Where nature&apos;s beauty and comfortable living blend seamlessly.
             Hidden away in the heart of the Italian Dolomites, this is your
             paradise away from home. But it&apos;s not just about the luxury
@@ -34,7 +39,7 @@ export default function Page() {
             This is where memorable moments are made, surrounded by
             nature&apos;s splendor. It&apos;s a place to slow down, relax, and
             feel the joy of being together in a beautiful setting.
-          </p> */}
+          </p>
         </div>
       </div>
 
@@ -59,7 +64,7 @@ export default function Page() {
         </h1>
 
         <div className="space-y-8">
-          {/* <p>
+          <p>
             Since 1962, The Wild Oasis has been a cherished family-run retreat.
             Started by our grandparents, this haven has been nurtured with love
             and care, passing down through our family as a testament to our
@@ -72,7 +77,7 @@ export default function Page() {
             guest; you&apos;re part of our extended family. So join us at The
             Wild Oasis soon, where tradition meets tranquility, and every visit
             is like coming home.
-          </p> */}
+          </p>
 
           <div>
             <Link
