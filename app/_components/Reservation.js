23531e1c0e1264acd9a473cdc2ecc6c667a3e21f
codehass <hassan.elouardy06@gmail.com>
2025-05-15 22:26:12 +0100

    add session to reservation

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -2,9 +2,13 @@ import React from 'react'
 import DateSelector from './DateSelector'
 import ReservationForm from './ReservationForm'
 import { getBookedDatesByCabinId, getSettings } from '../_lib/data-service'
+import { auth } from '../_lib/auth'
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([await getSettings(), getBookedDatesByCabinId(cabin.id)])
+  const session = await auth();
+
+  console.log(session);
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
