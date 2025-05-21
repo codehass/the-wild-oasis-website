2ce826e59f61737bef62e7f035bc6744fb0532b6
codehass <hassan.elouardy06@gmail.com>
2025-05-21 15:06:33 +0100

    updating the profile using server actions

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,57 +1,3 @@
-// import NextAuth from "next-auth";
-// // import GoogleProvider from "next-auth/providers/google";
-// import Google from "next-auth/providers/google";
-
-// const authConfig = {
-//   providers: [
-//     Google({
-//       clientId: process.env.AUTH_GOOGLE_ID,
-//       clientSecret: process.env.AUTH_GOOGLE_SECRET
-//     })
-//   ]
-// }
-
-// export const { auth, handlers: { GET, POST }, } = NextAuth(authConfig);
-
-// import { NextAuth } from "@auth/nextjs";
-
-// import Google from "@auth/core/providers/google";
-
-// const authConfig = {
-//   trustHost: true,
-//   providers: [
-//     Google({
-//       clientId: process.env.AUTH_GOOGLE_ID,
-//       clientSecret: process.env.AUTH_GOOGLE_SECRET,
-//     }),
-//   ],
-//   secret: process.env.AUTH_SECRET,
-// };
-
-// export const { auth, handlers } = NextAuth(authConfig);
-// console.log("handlers: ", handlers)
-// export const { GET, POST } = handlers;
-
-// app/auth.js
-// import Google from "@auth/core/providers/google";
-// import { NextAuth } from "@auth/nextjs";
-
-// export const {
-//   handlers: { GET, POST },
-//   auth,
-//   signIn,
-//   signOut
-// } = NextAuth({
-//   providers: [
-//     Google({
-//       clientId: process.env.AUTH_GOOGLE_ID,
-//       clientSecret: process.env.AUTH_GOOGLE_SECRET
-//     })
-//   ],
-//   secret: process.env.AUTH_SECRET,
-//   trustHost: true // Required for Vercel/localhost
-// });
-
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { createGuest, getGuest } from "./data-service";
@@ -87,7 +33,7 @@ const authConfig = {
   },
   pages: {
     signIn: "/login",
-  }
+  },
 };
 
 export const {
